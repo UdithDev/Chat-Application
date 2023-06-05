d0311126f2b63cae0ec95491229d278e7718d1e3
Udith_Dev_Wee <daveudith@gmail.com>
2023-06-06 02:16:20 +0530

    making server and clienthandler

---
+++ src/ClientHandler.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader; //This will be used to read data
+    private BufferedWriter bufferedWriter;
+    private String clientUserName;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUserName=bufferedReader.readLine();
+            clientHandlers.add(this);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
